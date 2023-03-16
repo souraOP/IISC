c996c85a0aee913f53ccb8aac6986a96aad77e93
DeepaMahm <deepamahm.iisc@gmail.com>
2023-03-16 09:02:51 +0530

    Update __init__.py

--- iisc/__init__.py
+++ iisc/__init__.py
@@ -6,7 +6,7 @@ SIMULATOR_DIR = BASE_DIR / ".." / "simulator" / "pancreas"
 SIMULATOR = SIMULATOR_DIR / "main.exe"
 RESULTS_DIR = BASE_DIR / "simulator" / "results" / "adv_disp" / "simgraph"
 UPLOAD_DIR = BASE_DIR / ".." / "upload"
-TASK_DIR = UPLOAD_DIR / "sample" / "task" # task dir
+TASK_DIR = UPLOAD_DIR / "sample" / "task_tumor_design2"  # task dir
 
 # graph input
 INPUT_XLSX = SIMULATOR_DIR / "input.xlsx"
