4c23430bad00a158b08e3919966781a15b8db23d
DeepaMahm <deepamahm.iisc@gmail.com>
2023-03-20 10:36:14 +0530

    zip results

--- iisc/simulator.py
+++ iisc/simulator.py
@@ -222,7 +222,19 @@ class Simulator:
         with open(self.r_json, 'w') as outfile:
             json.dump(d, outfile, indent=4)
 
+    def zip_results(self) -> None:
+    # get a list of all files in the folder
+    files = [os.path.join(self.data_path, f) for f in os.listdir(self.data_path) if
+             os.path.isfile(os.path.join(self.data_path, f))]
 
+    # create a new zip file
+    zip_file_path = os.path.join(self.data_path, 'results.zip')
+    with zipfile.ZipFile(zip_file_path, 'w') as zip:
+        # add each file to the zip file
+        for file in files:
+            zip.write(file, os.path.basename(file))
+
+            
 if __name__ == '__main__':
     sim = Simulator(data_path=TASK_DIR)
     sim.runsimulation() #task_dir=TASK_DIR)
